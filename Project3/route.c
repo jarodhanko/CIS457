b8b7399f7a7819f02db7f5cc5b699d7fa04b6a4f
Jarod Hanko <hankoj@eos04.cis.gvsu.edu>
2017-03-21 11:37:07 -0400

    message

--- Project3/route.c
+++ Project3/route.c
@@ -220,8 +220,12 @@ int main(){
 		printf("SOURCE: %02X", request2.ip_header.saddr);
 		printf("DESTINATION: %02X", request2.ip_header.daddr);
 
-		reply.ip_header.daddr = 0x0a010003;// request2.ip_header.saddr;
-		reply.ip_header.saddr = 0x0a010001; //request2.ip_header.daddr;
+		char *tmp3;
+		memcpy(tmp3, &request2.ip_header.saddr, 4);
+		reply.ip_header.daddr = *tmp3;
+		char *tmp4;
+		memcpy(tmp4, &request2.ip_header.daddr, 4);;
+		reply.ip_header.saddr = *tmp4; //request2.ip_header.daddr;
 
 		reply.icmp_header.type = ICMP_ECHOREPLY;
 		reply.icmp_header.checksum = 0;
