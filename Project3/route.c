0e7232464cd657c8150dd40b55a840e8ceac415b
Jarod Hanko <hankoj@dc07.cis.gvsu.edu>
2017-03-21 15:10:49 -0400

    message

--- Project3/route.c
+++ Project3/route.c
@@ -210,7 +210,8 @@ int main(){
 		reply.icmp_header.checksum = 0;
 
 		//reply.icmp_header.checksum = htons(ip_checksum(&reply.icmp_header, sizeof(reply.icmp_header));
-		reply.icmp_header.checksum = ip2_checksum(&reply.icmp_header, sizeof(reply.icmp_header));
+		printf("\n \t SIZEOFHEADER: %02X \n", sizeof(reply.icmp_header));
+		reply.icmp_header.checksum = htons(ip2_checksum(&reply.icmp_header, sizeof(reply.icmp_header)));
 
 
 		printf("\n IPHDR_len: %02X \n SIZEOF: %d \n", reply.ip_header.ihl, sizeof(reply));
