0e665969a52edeb947aeafdd4f79c7a8f3187eee
Jarod Hanko <hankoj@eos04.cis.gvsu.edu>
2017-03-21 08:48:30 -0400

    message

--- Project3/route.c
+++ Project3/route.c
@@ -225,6 +225,7 @@ int main(){
 
 		//reply.icmp_header.checksum = htons(ip_checksum(&reply.icmp_header, sizeof(reply.icmp_header)));
 		
+		printf("\n IPHDR_len: %02X \n SIZEOF: %d \n", reply.ip_header.ihl, sizeof(reply));
 		send(packet_socket, &reply, sizeof(reply), 0);
 	}
 
