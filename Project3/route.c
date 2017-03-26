108e98df6cda73eec9b01abeda5ec4b72e120c8a
Jarod Hanko <hankoj@eos04.cis.gvsu.edu>
2017-03-25 20:50:50 -0400

    message

--- Project3/route.c
+++ Project3/route.c
@@ -196,7 +196,7 @@ int main(){
 		int datalength = ntohs(request2.ip_header.tot_len) - sizeof(request2.ip_header) - sizeof(request2.icmp_header);
 		if(datalength > 0){
 			request2.data = (char*)malloc(datalength);
-			memcpy(&request2.data, &buf2 + sizeof(request2), datalength);
+			memcpy(&request2.data, buf2 + sizeof(request2), datalength);
 		}
 		printf("\n THE DATA LENGTH IS %d", sizeof(request2.data));
 		unsigned char tmp3[] = {buf2[26], buf2[27], buf2[28], buf2[29]};
