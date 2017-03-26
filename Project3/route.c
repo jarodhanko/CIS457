7f63f3cfa8eae985051edd551edf58ff8646c53c
Jarod Hanko <hankoj@eos04.cis.gvsu.edu>
2017-03-25 21:11:15 -0400

    message

--- Project3/route.c
+++ Project3/route.c
@@ -197,7 +197,7 @@ int main(){
 
 		if(datalength > 0){
 			request2.data = (char*)malloc(datalength);
-			//memcpy(&request2.data, buf2 + sizeof(request2), datalength);
+			memcpy(&request2.data, buf2 + sizeof(request2), datalength);
 		}
 		printf("\n THE DATA LENGTH IS %d", sizeof(request2.data));
 		unsigned char tmp3[] = {buf2[26], buf2[27], buf2[28], buf2[29]};
