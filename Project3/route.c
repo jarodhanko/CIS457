90d68c26d90dfd954d6a488f32a2fc9d12b8a5c7
Norman Cunningham <cunningn@eos22.cis.gvsu.edu>
2017-03-27 01:32:19 -0400

    the drive for satisfaction is dwindling

--- Project3/route.c
+++ Project3/route.c
@@ -229,6 +229,11 @@ int main(int argc, char **argv){
     		//this packet is incoming or outgoing (when using ETH_P_ALL, we
     		//see packets in both directions. Only outgoing can be seen when
     		//using a packet socket with some specific protocol)
+printf("Interface: %s \n", tempInterface->name);
+		printf("MAC  addr: %s \n", ether_ntoa((struct ether_addr*)tempInterface->mac_addrs));
+		printf("IP   addr: %02X.%02X.%02X.%02X \n",tempInterface->ip_addrs[0], tempInterface->ip_addrs[1],
+										  			 tempInterface->ip_addrs[2], tempInterface->ip_addrs[3]);
+		printf("sock_addr: %d \n", tempInterface->packet_socket);
     		int n = recvfrom(tempInterface->packet_socket, buf, 1500,0,(struct sockaddr*)&recvaddr, &recvaddrlen);
     		//ignore outgoing packets (we can't disable some from being sent
     		//by the OS automatically, for example ICMP port unreachable
