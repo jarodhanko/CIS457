ae08b6c72fcc49c2430eea29044d5745a24887b4
Norman Cunningham <cunningn@eos22.cis.gvsu.edu>
2017-03-27 18:23:29 -0400

    loading table

--- Project3/route.c
+++ Project3/route.c
@@ -365,7 +365,7 @@ void load_table(struct routing_table **rtable, char *filename){
 			item[--index] = '\0';
 			struct sockaddr_in sa;
 			inet_pton(AF_INET, item, &(sa.sin_addr));
-			(*rtable)->network = sa.sin_addr.s_addr;
+			(*rtable)->network = htons(sa.sin_addr.s_addr);
 			//inet_ntop(AF_INET, &(sa.sin_addr), (*rtable)->network, INET_ADDRSTRLEN);
 			index = 0;
 			break;
