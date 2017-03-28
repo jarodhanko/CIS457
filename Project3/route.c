bbdf93ccdcd8007d8e2726202b54b71da682fe13
Jarod Hanko <hankoj@eos04.cis.gvsu.edu>
2017-03-28 08:08:09 -0400

    changes

--- Project3/route.c
+++ Project3/route.c
@@ -279,10 +279,10 @@ int main(int argc, char **argv){
 			tempIcmp = ((struct iicmp*)&buf);
 
 			printf("ARP? %d \n", tempArp->arp_header.ea_hdr.ar_pro); 
-			printf("ICMP? %d \n", tempIcmp->eth_header.ether_type);
+			printf("ICMP? %d %d \n", ntohs(recvaddr.sll_protocol), ETH_P_IP);
 			
 			//limit based on mac address and ip address
-			if((ntohs(recvaddr.sll_protocol) == ETH_P_ARP) && n > -1 ){//&& tempArp->arp_header.ea_hdr.ar_pro == ARPOP_REQUEST){
+			if((ntohs(recvaddr.sll_protocol) == ETH_P_ARP) && n > -1 ) && tempArp->arp_header.ea_hdr.ar_op == ARPOP_REQUEST){
 				struct aarp *request;
 		
 				request = ((struct aarp*)&buf);
