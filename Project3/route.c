1a59bfe9b5866796b226b465e99cd502e00ad93f
Norman Cunningham <cunningn@eos22.cis.gvsu.edu>
2017-04-01 02:51:38 -0400

    the puzzle is coming together

--- Project3/route.c
+++ Project3/route.c
@@ -426,7 +426,7 @@ int main(int argc, char **argv){
 								// Copy the temp interface name to i_name, exit the loop.
 								memcpy(&name1A_INT, tmp1_INT->name, 7);
 								memcpy(name1P_INT, name1A_INT, 7);
-								printf("FIX ----- ME");
+								printf("FIX ----- ME\n");
 								break; 
 							}
 
