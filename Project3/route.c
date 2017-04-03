5d945e979ab5fd85e1fd241d59be4bedd485408d
Norman Cunningham <cunningn@eos22.cis.gvsu.edu>
2017-04-03 04:13:13 -0400

    almost done

--- Project3/route.c
+++ Project3/route.c
@@ -1047,7 +1047,71 @@ says it does not contain a full tcp header???
 									if (request_IICMP->ip_header.ttl == 1){
 
 										printf("ICMP - The packet died\n");
-										// ICMP error - packet died in our arms.
+										//#############################################################################	
+										// START: send ICMP error - ICMP_TIME_EXCEEDED
+										//#############################################################################
+
+										// Set ether header source and destination host.
+										memcpy(reply_IICMP.eth_header.ether_shost, prime_Interface->mac_addrs, 6);
+										memcpy(reply_IICMP.eth_header.ether_dhost, request_IICMP->eth_header.ether_shost, 6);
+
+										// Set ether header type.
+										reply_IICMP.eth_header.ether_type = htons(ETHERTYPE_IP);
+
+										// Set ip header destination and source address.
+										reply_IICMP.ip_header.daddr = request_IICMP->ip_header.saddr;
+										memcpy(&reply_IICMP.ip_header.saddr, prime_Interface->ip_addrs, 4);
+
+										// Set other ip header fields.
+										reply_IICMP.ip_header.check    = 0;
+										reply_IICMP.ip_header.frag_off = 0; 
+										reply_IICMP.ip_header.ihl      = 5;
+										reply_IICMP.ip_header.protocol = IPPROTO_ICMP; 
+										reply_IICMP.ip_header.tos      = 0;
+										reply_IICMP.ip_header.tot_len  = htons(28); 
+										reply_IICMP.ip_header.ttl      = 64;
+										reply_IICMP.ip_header.version  = 4;
+							
+										// Set icmp header type.
+										reply_IICMP.icmp_header.type = ICMP_TIME_EXCEEDED;
+
+										// Clear icmp header checksum.
+										reply_IICMP.icmp_header.checksum = 0;
+
+										// Set icmp error code.
+										reply_IICMP.icmp_header.code = ICMP_HOST_UNREACH;
+
+
+
+										// Calc the ip checksum.
+										reply_IICMP.ip_header.check = 0;
+										char data5[1500];
+										memcpy(data5, buf, 1500);
+										memcpy(data5 + sizeof(struct ether_header), &reply_IICMP.ip_header, sizeof(struct iphdr));
+										reply_IICMP.ip_header.check = ntohs(calculateIPChecksum(data5, n));
+
+
+										// Calculate icmp header checksum.
+										unsigned char *data;
+										int datalength = ntohs(request_IICMP->ip_header.tot_len) - 
+																	 sizeof(request_IICMP->ip_header) - 
+																	 sizeof(request_IICMP->icmp_header);
+										if(datalength > 0){
+											data = malloc(datalength);
+											memcpy(data, buf + sizeof(struct iicmp), datalength);
+										}
+										unsigned char ptr2[sizeof(reply_IICMP.icmp_header) + datalength];
+										memcpy(ptr2, &reply_IICMP.icmp_header, sizeof(reply_IICMP.icmp_header));
+										memcpy(ptr2 + sizeof(reply_IICMP.icmp_header), data, datalength);
+										reply_IICMP.icmp_header.checksum = icmp_checksum(&ptr2, sizeof(ptr2));
+
+
+										// Send the ICMP error on original interface.
+										send(prime_Interface->packet_socket, &reply_IICMP, sizeof(reply_IICMP), 0);
+
+										//#############################################################################	
+										// END: send ICMP error - ICMP_TIME_EXCEEDED
+										//#############################################################################
 // SEND ERROR???
 
 									}
